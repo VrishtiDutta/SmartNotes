802c63619226afb1e2b838f0694d4a64acd80712
Shirley Xia <xshirl1027@gmail.com>
2016-11-26 18:33:26 -0500

    nodes now draggable. We still need to update connections, so that they move with the nodes

--- front_end_with_commented_out_sections.py
+++ front_end_with_commented_out_sections.py
@@ -7,7 +7,7 @@ Press the circle button before clicking anywhere else: turn off node creation.
 Press the line button: click one node and then another node and will create a line between the two nodes.
 Press the line button before clicking the second node (or the first node): turn off line creation. 
 """
-
+import string
 from tkinter import *
 from smart_backend import *
 #from checker import check_cycle
@@ -17,6 +17,8 @@ global visual_graph_height
 global definition_list
 global def_list_string
 global graph
+global circ_list
+global selected_node
 graph = None
 visual_graph_height = None
 
@@ -37,9 +39,15 @@ class FrontEndNode(Node):
 
     def get_x(self):
         return self.x
-
+    
+    def set_x(self, x):
+        self.x = x
+    
     def get_y(self):
         return self.y
+    
+    def set_y(self, y):
+        self.y = y
 
     def get_i(self):
         return self.i
@@ -107,6 +115,7 @@ def display_text():
     global graph
     global def_list_string
     
+    default_text = '' #(9*13-len(E1.get(1.0, END))-21)*'.'
     if graph == None:
         graph = Graph(fnode)
     else:
@@ -114,7 +123,7 @@ def display_text():
     
     main_window.delete(text_entry_id)
     main_window.delete(text_button_id)
-    text_id = main_window.create_text(event_x, event_y, text=E1.get(1.0, END), width=95)
+    text_id = main_window.create_text(event_x, event_y+10, text=E1.get(1.0, END)+default_text, width=95, tags="token")
     fnode.set_text_id(text_id)
     fnode.set_text(E1.get(1.0, END))
     definition_list.parse_node(fnode)
@@ -141,7 +150,9 @@ def make_circle(event):
     if make_circle_switch == 1: 
         make_line_switch = 0
         del_switch = 0
-        arc = main_window.create_rectangle(event.x - 50, event.y - 50, event.x + 50, event.y + 50, outline="#ABA7A7", width=5, fill="#FFFFFF")
+        arc = main_window.create_rectangle(event.x - 50, event.y - 50, event.x + 50, event.y + 50, outline="#ABA7A7", width=5, fill="#FFFFFF", tags="token")
+        
+        
         fnode = FrontEndNode(event.x, event.y, arc)
 
         E1 = Text(main_window,height=5, width=10)
@@ -154,6 +165,7 @@ def make_circle(event):
         text_button_id = main_window.create_window(event.x, event.y+50, window=text_button)
     
         circ_list.append(fnode)
+        #print(circ_list)
         make_circle_switch = 0
 
     #Delete Circle
@@ -204,62 +216,6 @@ def make_circle(event):
         if make_line_switch > 3:
             make_line_switch = 1
             line_pos = []
-
-##            #If Invalid Connection
-##            if  will_create_cycle():
-##                tp = Toplevel()
-##                tp.title("Error")
-##
-##                msg = Message(tp, text="Lorelai Gilmore is disappointed in you, you were going to create a cycle, please don't do that.")
-##                msg.pack()
-##
-##                but = Button(tp, text="Dismiss", command = tp.destroy)
-##                but.pack()
-##                
-##                make_line_switch = 0
-##                line_pos = []
-##
-##            #If Valid Connection
-##            else:
-            
-                
-##def plus_level():
-##    global plus_minus_list
-##    global graph_height
-##    global visual_graph_height
-##    global circ_list
-##
-##    if visual_graph_height == None:
-##        visual_graph_height = graph_height
-##
-##    if visual_graph_height == 0:
-##        pass
-##    else:
-##        visual_graph_height = visual_graph_height - 1
-##        level_one = []
-##        for cir in circ_list:
-##            if cir.get_height() == visual_graph_height:
-##                level_one.append(cir)
-##
-##        for i in range(len(level_one)):
-##            for line in circ_list[i].get_connections():
-##                main_window.delete(line)
-##            main_window.delete(circ_list[i].get_i())
-##            main_window.delete(circ_list[i].get_text_id())
-##            graph.delete(circ_list[i])
-##
-##            graph_height = graph.get_height()
-##
-##            for item in circ_list:
-##                ind_list = []
-##                for j in range(len(item.get_pmlist())):
-##                    if item.get_pmlist()[j].get_i() == circ_list[i].get_i():
-##                        ind_list.append(j)
-##
-##                for ind in ind_list:
-##                    item.pop_pmlist(ind)
-##                
-##            circ_list.remove(level_one[i])
         
     
 """Check if this connection is valid in the graph"""
@@ -291,12 +247,57 @@ def send_text_to_list():
     main_window.delete(def_list_string)
     def_list_string = main_window.create_text(960, 60, anchor=NW, text=definition_list.string(), font=("Times", 12), width=320)
     
+def find_selected_node(event, circ_list):
+    for cir in circ_list:
+                if is_in_node(event.x, event.y, cir.get_x(), cir.get_y()):
+                    return cir
+    
+def OnTokenButtonPress(event):
+    '''Being drag of an object'''
+    
+    # record the item and its location
+    if(len(main_window.find_overlapping(event.x-25, event.y-25, event.x+25, event.y+25))==2):
+        main_window.drag_data["items"] = main_window.find_overlapping(event.x-25, event.y-25, event.x+25, event.y+25)
+    main_window.drag_data["x"] = event.x
+    main_window.drag_data["y"] = event.y
+    selected_node = find_selected_node(event, circ_list)
+    
+
+def OnTokenButtonRelease(event):
+    '''End drag of an object'''
+    # reset the drag information
+    main_window.drag_data["items"] = None
+    main_window.drag_data["x"] = 0
+    main_window.drag_data["y"] = 0
+
+def OnTokenMotion(event):
+    
+    selected_node = find_selected_node(event, circ_list)
+    if(selected_node != None):
+        print(selected_node.string())    
+    '''Handle dragging of an object'''
+    # compute how much this object has moved
+    delta_x = event.x - main_window.drag_data["x"]
+    delta_y = event.y - main_window.drag_data["y"]
+    # move the object the appropriate amount
+    if(main_window.drag_data["items"]!=None):
+        main_window.move(main_window.drag_data["items"][0], delta_x, delta_y)
+        main_window.move(main_window.drag_data["items"][1], delta_x, delta_y)
+    # record the new position
+    main_window.drag_data["x"] = event.x
+    main_window.drag_data["y"] = event.y
+    
+    if(selected_node!=None):
+        #print(selected_node.get_x()+delta_x +"assadsadssadk"+ selected_node.get_y()+delta_y)
+        selected_node.set_x(selected_node.get_x()+delta_x)
+        selected_node.set_y(selected_node.get_y()+delta_y)
+
 if __name__ == "__main__":
 
     global Eterm
     global Edef
     global circ_list
-    
+    global main_window
     #Initialize Window
     top = Tk()
     
@@ -304,14 +305,15 @@ if __name__ == "__main__":
     main_window = Canvas(top, bg="#FFFFFF", height=7000, width=1300)
     main_window.pack()
     def_list_string = main_window.create_text(500, 60, anchor=NW, text=definition_list.string(), font=("Times", 12), width=220)
-
+    main_window.drag_data = {"x": 0, "y": 0, "items": None}
     #Global Variables
     circ_list = []
     make_circle_switch = 0
     make_line_switch = 0
     del_switch = 0
     line_pos = []
-
+    selected_node = None
+    
     #Initalize Button Images
     line_photo = PhotoImage(file="arrow.png")
     plus_photo = PhotoImage(file="plus.png")
@@ -365,5 +367,8 @@ if __name__ == "__main__":
     
     #Run Program
     main_window.bind("<Button-1>", make_circle)
+    main_window.tag_bind("token", "<ButtonPress-1>", OnTokenButtonPress)
+    main_window.tag_bind("token", "<ButtonRelease-1>", OnTokenButtonRelease)
+    main_window.tag_bind("token", "<B1-Motion>", OnTokenMotion)     
     main_window.pack()
     top.mainloop()
